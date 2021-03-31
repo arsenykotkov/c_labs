77d66237b856f11d66d1b550a066a86a1a112c87
arseniikotkov <81694793+arseniikotkov@users.noreply.github.com>
2021-04-01 01:28:04 +1000

    Delete lab5 directory

--- lab5/matrix.h
+++
@@ -1,8 +0,0 @@
-#ifndef MATRIX_H
-#define MATRIX_H
-
-double **sum(int N, double **matrix1, double **matrix2);
-double **difference(int N, double **matrix1, double **matrix2);
-double **multiplication(int N, double **matrix1, double **matrix2);
-
-#endif
